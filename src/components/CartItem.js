e243f613e6c486250d9454a812ddca3eceeb1adc
Kabilan P <104247633+Kabilanp4@users.noreply.github.com>
2025-02-02 23:38:07 +0530

    Added modal features

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,21 +1,48 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import {
+  decreaseItem,
+  increaseItem,
+  removeItem,
+} from "../features/cart/cartSlice";
 import { ChevronDown, ChevronUp } from "../icons";
-
 const CartItem = ({ id, img, title, price, amount }) => {
+  const dispatch = useDispatch();
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">${price}</h4>
-        <button className="remove-btn">remove</button>
+        <button
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(id));
+          }}
+        >
+          remove
+        </button>
       </div>
       <div>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            dispatch(increaseItem(id));
+          }}
+        >
           <ChevronUp />
         </button>
         <p className="amount">{amount}</p>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decreaseItem(id));
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -15,8 +15,40 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increaseItem: (state, action) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decreaseItem: (state, action) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotal: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
   },
 });
 
-export const { clearCart } = cartSlice.actions;
+export const {
+  clearCart,
+  removeItem,
+  increaseItem,
+  decreaseItem,
+  calculateTotal,
+} = cartSlice.actions;
 export default cartSlice.reducer;
